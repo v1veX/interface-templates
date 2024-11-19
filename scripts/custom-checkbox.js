db0eec91afcff5e1b402f3d3e495b003a76e05f4
v1veX <sakmarkin.va@yandex.ru>
2024-11-19 20:35:58 +1000

    modify fouth component

--- scripts/custom-checkbox.js
+++ scripts/custom-checkbox.js
@@ -5,17 +5,29 @@ class CustomCheckbox {
     _selectors = {
         nativeInputElement: '[data-js-checkbox-native]',
         controlElement: '[data-js-checkbox-control]',
-    }
+    };
 
-    _keyCodes = ['Enter', 'Space']
+    _keyCodes = ['Enter', 'Space'];
 
-    constructor(customElementSelector) {
-        const wrapperElement = document.querySelector(customElementSelector);
+    constructor(wrapperElementSelector, isCheked = false) {
+        const wrapperElement = document.querySelector(wrapperElementSelector);
 
         this._nativeInputElement = wrapperElement.querySelector(this._selectors.nativeInputElement);
         this._controlElement = wrapperElement.querySelector(this._selectors.controlElement);
 
+        this._nativeInputElement.checked = isCheked;
+
         this._bindEvents();
+    }
+
+    /**
+     * [Public]
+     * 
+     * Changes state of the checkbox.
+     * @param {boolean} check sets when it needs to set exact state. 
+    */
+    changeState(check = !this._nativeInputElement.checked) {
+        this._nativeInputElement.checked = check;
     }
 
     _onKeyDown = (event) => {
@@ -23,14 +35,12 @@ class CustomCheckbox {
 
         event.preventDefault();
 
-        const isNativeChecked = this._nativeInputElement.checked;
-
-        this._nativeInputElement.checked = !isNativeChecked;
+        this.changeState();
     }
 
     _bindEvents() {
-        this._controlElement.addEventListener('keydown', this._onKeyDown)
+        this._controlElement.addEventListener('keydown', this._onKeyDown);
     }
 }
 
-new CustomCheckbox('#custom-checkbox-1')
+new CustomCheckbox('#custom-checkbox-1', true);
